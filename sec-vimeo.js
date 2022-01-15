ede1134a122cc94f021f274a1cc722680fb83301
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-01-15 14:42:49 +0200

    Create sec-vimeo.js

---
+++ sec-vimeo.js
@@ -0,0 +1,609 @@
+setTimeout(() => { 
+{
+  document.getElementById("s1p1button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s1p1video").show();
+    $("#s1p1button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s1p2button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s1p2video").show();
+    $("#s1p2button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s1p3button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s1p3video").show();
+    $("#s1p3button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s1p4button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s1p4video").show();
+    $("#s1p4button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s2p1button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s2p1video").show();
+    $("#s2p1button").css("background-color", "#e7edf0");
+  });
+}
+{
+  document.getElementById("s2p2button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s2p2video").show();
+    $("#s2p2button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s2p3button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s2p3video").show();
+    $("#s2p3button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s2p4button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s2p4quiz").show();
+    $("#s2p4button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s3p1button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s3p1video").show();
+    $("#s3p1button").css("background-color", "#e7edf0");
+  });
+}
+{
+  document.getElementById("s3p2button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s3p2video").show();
+    $("#s3p2button").css("background-color", "#e7edf0");
+  });
+}
+{
+  document.getElementById("s3p3button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s3p3video").show();
+    $("#s3p3button").css("background-color", "#e7edf0");
+  });
+}
+{
+  document.getElementById("s3p4button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s3p4video").show();
+    $("#s3p4button").css("background-color", "#e7edf0");
+  });
+}
+{
+  document.getElementById("s3p5button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s3p5video").show();
+    $("#s3p5button").css("background-color", "#e7edf0");
+  });
+}
+{
+  document.getElementById("s3p6button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s3p6quiz").show();
+    $("#s3p6button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s4p1button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s4p1video").show();
+    $("#s4p1button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s4p2button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s4p2video").show();
+    $("#s4p2button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s4p3button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s4p3quiz").show();
+    $("#s4p3button").css("background-color", "#e7edf0");
+  });
+}
+{
+  document.getElementById("s5p1button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s5p1video").show();
+    $("#s5p1button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s6p1button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s6p1video").show();
+    $("#s6p1button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s6p2button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s6p2video").show();
+    $("#s6p2button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s6p3button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s6p3video").show();
+    $("#s6p3button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s6p4button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s6p4video").show();
+    $("#s6p4button").css("background-color", "#e7edf0");
+  });
+}
+
+{
+  document.getElementById("s6p5button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#s6p5quiz").show();
+    $("#s6p5button").css("background-color", "#e7edf0");
+  });
+}
+
+
+const checkcompleted1 = function() {
+let prognum = 0;
+    if (document.querySelector("#s1p1x").style.display == "block") {
+      prognum++;
+    }
+    if (document.querySelector("#s1p2x").style.display == "block") {
+      prognum++;
+    }
+    if (document.querySelector("#s1p3x").style.display == "block") {
+      prognum++;
+    }
+    if (document.querySelector("#s1p4x").style.display == "block") {
+      prognum++;
+    }
+    document.querySelector("#prognum").textContent = prognum;
+}
+
+const checkcompleted2 = function() {
+let prognum2 = 0;
+    if (document.querySelector("#s2p1x").style.display == "block") {
+      prognum2++;
+    }
+    if (document.querySelector("#s2p2x").style.display == "block") {
+      prognum2++;
+    }
+    if (document.querySelector("#s2p3x").style.display == "block") {
+      prognum2++;
+    }
+    if (document.querySelector("#s2p4x").style.display == "block") {
+      prognum2++;
+    }
+    document.querySelector("#prognum2").textContent = prognum2;
+}
+
+const checkcompleted3 = function() {
+let prognum3 = 0;
+    if (document.querySelector("#s3p1x").style.display == "block") {
+      prognum3++;
+    }
+    if (document.querySelector("#s3p2x").style.display == "block") {
+      prognum3++;
+    }
+    if (document.querySelector("#s3p3x").style.display == "block") {
+      prognum3++;
+    }
+    if (document.querySelector("#s3p4x").style.display == "block") {
+      prognum3++;
+    }
+    if (document.querySelector("#s3p5x").style.display == "block") {
+      prognum3++;
+    }
+    if (document.querySelector("#s3p6x").style.display == "block") {
+      prognum3++;
+    }
+document.querySelector("#prognum3").textContent = prognum3;
+}
+
+const checkcompleted4 = function() {
+let prognum4 = 0;
+    if (document.querySelector("#s4p1x").style.display == "block") {
+      prognum4++;
+    }
+    if (document.querySelector("#s4p2x").style.display == "block") {
+      prognum4++;
+    }
+    if (document.querySelector("#s4p3x").style.display == "block") {
+      prognum4++;
+    }
+document.querySelector("#prognum4").textContent = prognum4;
+}
+
+const checkcompleted5 = function() {
+let prognum5 = 0;
+    if (document.querySelector("#s5p1x").style.display == "block") {
+      prognum5++;
+    }
+document.querySelector("#prognum5").textContent = prognum5;
+}
+const checkcompleted6 = function() {
+let prognum6 = 0;
+    if (document.querySelector("#s6p1x").style.display == "block") {
+      prognum6++;
+    }
+    if (document.querySelector("#s6p2x").style.display == "block") {
+      prognum6++;
+    }
+    if (document.querySelector("#s6p3x").style.display == "block") {
+      prognum6++;
+    }
+    if (document.querySelector("#s6p4x").style.display == "block") {
+      prognum6++;
+    }
+    if (document.querySelector("#s6p5x").style.display == "block") {
+      prognum6++;
+    }
+
+document.querySelector("#prognum6").textContent = prognum6;
+}
+
+
+{
+  var iframe = document.querySelector("#s1p1video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s1p1").hide();
+    $("#s1p1x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s1p1: true }, { merge: true });
+    checkcompleted1()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s1p2video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s1p2").hide();
+    $("#s1p2x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s1p2: true }, { merge: true });
+    checkcompleted1()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s1p3video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s1p3").hide();
+    $("#s1p3x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s1p3: true }, { merge: true });
+    checkcompleted1()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s1p4video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s1p4").hide();
+    $("#s1p4x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s1p4: true }, { merge: true });
+    checkcompleted1()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s2p1video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s2p1").hide();
+    $("#s2p1x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s2p1: true }, { merge: true });
+    checkcompleted2()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s2p2video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s2p2").hide();
+    $("#s2p2x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s2p2: true }, { merge: true });
+    checkcompleted2()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s2p3video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s2p3").hide();
+    $("#s2p3x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s2p3: true }, { merge: true });
+    checkcompleted2()
+  };
+  player.on("ended", finished);
+}
+
+
+{
+  var iframe = document.querySelector("#s3p1video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s3p1").hide();
+    $("#s3p1x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s3p1: true }, { merge: true });
+    checkcompleted3()
+  };
+  player.on("ended", finished);
+}
+
+
+{
+  var iframe = document.querySelector("#s3p2video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s3p2").hide();
+    $("#s3p2x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s3p2: true }, { merge: true });
+    checkcompleted3()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s3p3video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s3p3").hide();
+    $("#s3p3x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s3p3: true }, { merge: true });
+    checkcompleted3()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s3p4video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s3p4").hide();
+    $("#s3p4x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s3p4: true }, { merge: true });
+    checkcompleted3()
+  };
+  player.on("ended", finished);
+}
+
+{
+  var iframe = document.querySelector("#s3p5video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s3p5").hide();
+    $("#s3p5x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s3p5: true }, { merge: true });
+    checkcompleted3()
+  };
+  player.on("ended", finished);
+}  
+
+{
+  var iframe = document.querySelector("#s4p1video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s4p1").hide();
+    $("#s4p1x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s4p1: true }, { merge: true });
+    checkcompleted4()
+  };
+  player.on("ended", finished);
+}  
+
+{
+  var iframe = document.querySelector("#s4p2video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s4p2").hide();
+    $("#s4p2x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s4p2: true }, { merge: true });
+    checkcompleted4()
+  };
+  player.on("ended", finished);
+}  
+
+{
+  var iframe = document.querySelector("#s5p1video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s5p1").hide();
+    $("#s5p1x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s5p1: true }, { merge: true });
+    checkcompleted5()
+  };
+  player.on("ended", finished);
+}  
+
+{
+  var iframe = document.querySelector("#s6p1video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s6p1").hide();
+    $("#s6p1x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s6p1: true }, { merge: true });
+    checkcompleted6()
+  };
+  player.on("ended", finished);
+}  
+
+{
+  var iframe = document.querySelector("#s6p2video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s6p2").hide();
+    $("#s6p2x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s6p2: true }, { merge: true });
+    checkcompleted6()
+  };
+  player.on("ended", finished);
+}  
+
+{
+  var iframe = document.querySelector("#s6p3video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s6p3").hide();
+    $("#s6p3x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s6p3: true }, { merge: true });
+    checkcompleted6()
+  };
+  player.on("ended", finished);
+}  
+
+{
+  var iframe = document.querySelector("#s6p4video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#s6p4").hide();
+    $("#s6p4x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ s6p4: true }, { merge: true });
+    checkcompleted6()
+  };
+  player.on("ended", finished);
+}  
+
+}, 2000);
