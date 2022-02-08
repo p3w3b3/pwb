6a0314902c2114c34561e23d03fabe67fe1e7b11
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-02-08 15:22:37 +0200

    Create fb-stonegate.js

---
+++ training/deliverect/fb-stonegate.js
@@ -0,0 +1,119 @@
+{
+
+const email = document.querySelector('#email'); 
+const signOutButton = document.querySelector('#signOutButton');
+const userName = document.querySelector('#userName');
+const userName2 = document.querySelector('#userName2');
+const loader = document.querySelector('#loadanimation');
+const maincontent = document.querySelector('#maincontent');
+
+
+
+// Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyDApbEuGdjDf-dBlOljBvvXAPlZmUx-D5M",
+    authDomain: "stonegate-system.firebaseapp.com",
+    projectId: "stonegate-system",
+    storageBucket: "stonegate-system.appspot.com",
+    messagingSenderId: "798322604618",
+    appId: "1:798322604618:web:fada6a2e9f09f0319bc4b3",
+    measurementId: "G-1J8XNVRV7Q"
+  };
+
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const authChanged = firebase.auth().onAuthStateChanged(user =>{
+let prognum = 0
+
+const signout = () => {
+    auth.signOut();
+}
+
+signOutButton.addEventListener('click', signout);
+
+
+auth.onAuthStateChanged(async (user) => {
+  let currentPath = window.location.pathname;
+  if (user) {
+    maincontent.style.display='flex';
+		loader.style.display='none';
+    let myFS = firebase.firestore()
+    let docSnap = await myFS.doc("users/"+firebase.auth().currentUser.uid).get();
+    let data = docSnap.data()
+    let prereq = data['prereq'];
+      let name = data['Name'];
+      let emaild = data['Email']; 
+      	  let firstletter = name.substring(0, 1)
+        $('.firstletter').html(firstletter);
+    let s1p1 = data['d1p1'];    
+    let s1p2 = data['d1p2'];    
+    let s1p3 = data['d1p3'];    
+    let s1p4 = data['d1p4']; 
+
+
+    if( s1p1 == true) {
+$('#s1p1').hide()
+$('#s1p1x').show()
+prognum++
+}
+    if( s1p2 == true) {
+$('#s1p2').hide()
+$('#s1p2x').show()
+prognum++
+}
+
+    if( s1p3 == true) {
+$('#s1p3').hide()
+$('#s1p3x').show()
+prognum++
+}
+
+    if( s1p4 == true) {
+$('#s1p4').hide()
+$('#s1p4x').show()
+prognum++
+}
+
+
+let sp =((prognum)/4*100)
+let spn = Math.round(sp)+'%'
+firebase.firestore().doc("users/"+firebase.auth().currentUser.uid)
+  .set({deliverect:spn}, {merge:true})
+
+
+ if(prereq !== true) {
+        location.href = '/onboarding';
+        }
+ if(!!name) {
+      $('#userName2').html(name);
+  } else {
+  
+  firebase.firestore().doc("users/"+firebase.auth().currentUser.uid)
+  .set({Name:user.displayName}, {merge:true})
+  {
+  setTimeout(() => { 
+         window.location.href = '/dashboard/training/deliverect';
+  
+  }, 2000);
+  
+  }
+  
+      $('#userName2').html(name);
+  }
+
+  if(!!emaild) {
+      $('#email').html(emaild);
+  }} else {
+    console.log('user logged out');
+    signOutButton.style.display="none";
+    if('/' != currentPath){location.href = '/login';}
+    maincontent.style.display='none';
+		loader.style.display='flex';
+  }
+signOutButton.addEventListener('click', signout);
+document.querySelector('#prognum').textContent = prognum
+
+})})
+
+}
