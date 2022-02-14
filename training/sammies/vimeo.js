4bc68bf15e30a68ad42f37d27cada8a95de71319
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-02-14 12:55:36 +0200

    Create vimeo.js

---
+++ training/sammies/vimeo.js
@@ -0,0 +1,35 @@
+setTimeout(() => { 
+{
+  document.getElementById("sm1p1button").addEventListener("click", (event) => {
+    $(".section1selection").css("background-color", "white");
+    $(".section1div").hide();
+    $("#sm1p1video").show();
+    $("#sm1p1button").css("background-color", "#e7edf0");
+  });
+}
+
+const checkcompleted1 = function() {
+let prognum = 0;
+    if (document.querySelector("#sm1p1x").style.display == "block") {
+      prognum++;
+    }
+    document.querySelector("#prognumsm").textContent = prognumsm;
+}
+
+{
+  var iframe = document.querySelector("#sm1p1video");
+  var player = new Vimeo.Player(iframe);
+  var finished = function () {
+    videoended = "true";
+    $("#sm1p1").hide();
+    $("#sm1p1x").show();
+    firebase
+      .firestore()
+      .doc("users/" + firebase.auth().currentUser.uid)
+      .set({ sm1p1: true }, { merge: true });
+    checkcompleted1()
+  };
+  player.on("ended", finished);
+}
+  
+}, 2000);
