6f37c67b5dabfd5957a8a96d2e5cdee97c7901b1
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-02-15 10:59:50 +0200

    Create fb-stonegate.js

---
+++ training/verygoodburger/fb-stonegate.js
@@ -0,0 +1,94 @@
+{
+
+const email = document.querySelector('#email'); 
+const signOutButton = document.querySelector('#signOutButton');
+const userName = document.querySelector('#userName');
+const userName2 = document.querySelector('#userName2');
+const loader = document.querySelector('#loadanimation');
+const maincontent = document.querySelector('#maincontent');
+
+
+// Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyDApbEuGdjDf-dBlOljBvvXAPlZmUx-D5M",
+    authDomain: "stonegate-system.firebaseapp.com",
+    projectId: "stonegate-system",
+    storageBucket: "stonegate-system.appspot.com",
+    messagingSenderId: "798322604618",
+    appId: "1:798322604618:web:fada6a2e9f09f0319bc4b3",
+    measurementId: "G-1J8XNVRV7Q"
+  };
+
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const authChanged = firebase.auth().onAuthStateChanged(user =>{
+let prognumv = 0
+
+const signout = () => {
+    auth.signOut();
+}
+
+signOutButton.addEventListener('click', signout);
+
+
+auth.onAuthStateChanged(async (user) => {
+  let currentPath = window.location.pathname;
+  if (user) {
+    maincontent.style.display='flex';
+		loader.style.display='none';
+    let myFS = firebase.firestore()
+    let docSnap = await myFS.doc("users/"+firebase.auth().currentUser.uid).get();
+    let data = docSnap.data()
+    let prereq = data['prereq'];
+      let name = data['Name'];
+      let emaild = data['Email']; 
+      	  let firstletter = name.substring(0, 1)
+        $('.firstletter').html(firstletter);
+    let v1p1 = data['v1p1'];    
+
+
+    if( v1p1 == true) {
+$('#v1p1').hide()
+$('#v1p1x').show()
+prognumv++
+}
+
+let sp =((prognumv)/1*100)
+let spn = Math.round(sp)+'%'
+firebase.firestore().doc("users/"+firebase.auth().currentUser.uid)
+  .set({verygoodburger:spn}, {merge:true})
+
+
+ if(!!name) {
+      $('#userName2').html(name);
+  } else {
+  
+  firebase.firestore().doc("users/"+firebase.auth().currentUser.uid)
+  .set({Name:user.displayName}, {merge:true})
+  {
+  setTimeout(() => { 
+         window.location.href = '/dashboard/training/very-good-burger';
+  
+  }, 2000);
+  
+  }
+  
+      $('#userName2').html(name);
+  }
+
+  if(!!emaild) {
+      $('#email').html(emaild);
+  }} else {
+    console.log('user logged out');
+    signOutButton.style.display="none";
+    if('/' != currentPath){location.href = '/';}
+    maincontent.style.display='none';
+		loader.style.display='flex';
+  }
+signOutButton.addEventListener('click', signout);
+document.querySelector('#prognumv').textContent = prognumv
+
+})})
+
+}
