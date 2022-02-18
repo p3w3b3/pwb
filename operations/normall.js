4f264743da41159be1eea5b20bd6774212c8c20b
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-02-18 14:52:13 +0200

    Update and rename normalz.js to normall.js

--- operations/normall.js
+++ operations/normall.js
@@ -20,17 +20,3 @@ $('#ops4').addClass("cur")
 $('.nav-selection').removeClass("selected");
 $('#myan').addClass("selected");
 })
-
-
-{
-let obdata = "{{wf {&quot;path&quot;:&quot;fom-data&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}"
-var obdt = obdata.split('|');
-let fomname = obdt[0]
-let fomemail = obdt[1]
-let fomnumber = obdt[2]
-let fomphoto = obdt[3]
-
-document.getElementById("obname").textContent = fomname
-document.getElementById("obemail").textContent = fomemail
-document.getElementById("obphoto").src=fomphoto
-}
