49e3fd2e33906dbde283369ff31a1c7e19ce373f
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-02-18 12:23:14 +0200

    Update and rename normal-partner.js to normalpr.js

--- operations/normalpr.js
+++ operations/normalpr.js
@@ -1,14 +1,15 @@
 document.querySelector("#ops4").addEventListener("click", function() {
 $('.main-section-ops').hide()
-$('#db3').show()
+$('#db4').show()
 $('.generic-heading-ops').removeClass("cur");
 $('#ops4').addClass("cur");
 })
 
 
+
 document.querySelector("#myan").addEventListener("click", function() {
 $('.main-section-ops').hide()
-$('#db3').show()
+$('#db4').show()
 
 $('.sub-nav').hide()
 $('#sb3').css("display", "flex");
