ab80b1e55e3d3b8fd51e9feac3ca91f65ffeb518
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-02-27 09:52:15 +0200

    Update and rename fb66n.js to fb67.js

--- operations/fb67.js
+++ operations/fb67.js
@@ -360,10 +360,14 @@ window.intercomSettings = {
 // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/e84ncf6y'
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e84ncf6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 }
-
+	
+	
+$( document ).ready(function() {
 maincontent.style.display = 'flex'
 loader.style.display = 'none'
-      
+}); 
+	
+	
 // if gsign and no name   
 if(!!name) {
 $('#userName').html(name);
