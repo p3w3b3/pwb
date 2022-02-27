e20540369891b5148edab784478686f1bab1fb7f
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-02-27 09:25:15 +0200

    Update and rename fb66.js to fb66n.js

--- operations/fb66n.js
+++ operations/fb66n.js
@@ -361,7 +361,8 @@ window.intercomSettings = {
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e84ncf6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 }
 
-
+maincontent.style.display = 'flex'
+loader.style.display = 'none'
       
 // if gsign and no name   
 if(!!name) {
