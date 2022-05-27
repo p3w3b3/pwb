4123cdc1b2e474e8773a9755ddf3ffa4e18a5c8c
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-05-27 08:35:34 +0300

    Update and rename fb76-stg.js to fb77-stg.js

--- operations/fb77-stg.js
+++ operations/fb77-stg.js
@@ -190,6 +190,46 @@ $('#mbox4').css("display", "flex");
 
 $('.mobile-menu-wrapper').click()
 })
+	
+	
+document.getElementById("mbclucknrun").addEventListener("click", () => {
+$('.mobile-menu-button').removeClass( "current")
+$('#mbclucknrun').addClass( "current")
+
+$('.main-section-ops').hide()
+$('#dbcluck').css("display", "flex");
+$('.bottom-bar-dsb').hide()
+$('.middle-box').hide()
+$('#mbox4').css("display", "flex");
+
+$('.mobile-menu-wrapper').click()
+})
+
+document.getElementById("mblockednloaded").addEventListener("click", () => {
+$('.mobile-menu-button').removeClass( "current")
+$('#mblockednloaded').addClass( "current")
+
+$('.main-section-ops').hide()
+$('#dbloaded').css("display", "flex");
+$('.bottom-bar-dsb').hide()
+$('.middle-box').hide()
+$('#mbox4').css("display", "flex");
+
+$('.mobile-menu-wrapper').click()
+})
+
+document.getElementById("mbnomnyam").addEventListener("click", () => {
+$('.mobile-menu-button').removeClass( "current")
+$('#mbnomnyam').addClass( "current")
+
+$('.main-section-ops').hide()
+$('#dbnomnyam').css("display", "flex");
+$('.bottom-bar-dsb').hide()
+$('.middle-box').hide()
+$('#mbox4').css("display", "flex");
+
+$('.mobile-menu-wrapper').click()
+})
 
 
 document.getElementById("mbtraining2").addEventListener("click", () => {
