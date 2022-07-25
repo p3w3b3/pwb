5bace9a8d581233fd0d02a6513e2ffce2d3af93a
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-07-26 02:22:15 +0900

    Update and rename fb83.js to fb84.js

--- operations/fb84.js
+++ operations/fb84.js
@@ -280,6 +280,8 @@
   // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/e84ncf6y'
   (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e84ncf6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
   }
+    
+
   
   // if gsign and no name   
   if(!!name) {
@@ -538,3 +540,11 @@
   }); 
   }
   
+{
+window.addEventListener("load", function(){
+Intercom('onShow', function() {
+mixpanel.track("Intercom Chat - Opened")
+})
+});
+
+}
