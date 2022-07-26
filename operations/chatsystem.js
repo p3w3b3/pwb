fa755c3b104bb3c7696b2cf4754dc524ed8ef534
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-07-26 20:29:25 +0900

    Update chatsystem.js

--- operations/chatsystem.js
+++ operations/chatsystem.js
@@ -2,15 +2,14 @@ window.GLR = {
 appId: 'efb00929-7682-4b9c-b533-1cf2001dc8c6'
 };
 
-setTimeout(() => {
 (function(w, d, s){
 var j = document.createElement(s); j.async = 1; j.type = 'text/javascript'; j.src = 'https://widget.eu.karehq.com/latest.js';
 w.GLR = w.GLR || {};
 d.getElementsByTagName("head")[0].appendChild(j);
 })(window, document, "script");
-
+setTimeout(() => {
 kare.onEscalate(function(callbackEvent, messageBody) {
 Intercom('show');
 kare.close()
 });
-}, "5000")
+}, "1000")
