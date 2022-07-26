e7068e672194620d0d6c37b72ab989a4fe3a70f2
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-07-26 20:41:55 +0900

    Create chat-stg.js

---
+++ operations/chat-stg.js
@@ -0,0 +1,23 @@
+setTimeout(() => {
+
+$(document).ready(function(){
+
+window.GLR = {
+appId: 'efb00929-7682-4b9c-b533-1cf2001dc8c6'
+};
+
+(function(w, d, s){
+var j = document.createElement(s); j.async = 1; j.type = 'text/javascript'; j.src = 'https://widget.eu.karehq.com/latest.js';
+w.GLR = w.GLR || {};
+d.getElementsByTagName("head")[0].appendChild(j);
+})(window, document, "script");
+setTimeout(() => {
+kare.onEscalate(function(callbackEvent, messageBody) {
+Intercom('show');
+kare.close()
+});
+}, "2000")
+
+})
+
+}, "10000")
